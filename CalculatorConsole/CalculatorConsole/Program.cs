4a85c2830a9bc2e45f972d03874706c87362b14e
ARVAZAKI <arvazf09@gmail.com>
2022-09-28 20:19:10 +0700

    first commit

---
+++ CalculatorConsole/CalculatorConsole/Program.cs
@@ -0,0 +1,59 @@
+﻿
+using System.Globalization;
+
+namespace CalculatorConsole
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            double total;
+            Console.WriteLine("---- Console Calclator by @vazakii ----");
+            Console.WriteLine("======================================");
+            Console.WriteLine("note : Metode : perkalian/pembagian/penjumlahan/pengurangan/hasil bagi");
+            Console.WriteLine("======================================");
+
+            Console.Write("Masukkan angka pertama : ");
+            string a = Console.ReadLine();
+
+            Console.Write("Masukkan angka kedua : ");
+            string b = Console.ReadLine();
+
+            Console.Write("Metode : ");
+            string method = Console.ReadLine();
+
+            switch (method)
+            {
+                case "perkalian":
+                    total = Convert.ToDouble(a) * Convert.ToDouble(b);
+                    Console.WriteLine("Hasil = {0}",total);
+                    break;
+                case "pembagian":
+                    total = Convert.ToDouble(a) / Convert.ToDouble(b);
+                    Console.WriteLine("Hasil = {0}", total);
+                    break;
+                case "penjumlahan":
+                    total = Convert.ToDouble(a) + Convert.ToDouble(b);
+                    Console.WriteLine("Hasil = {0}", total);
+                    break;
+                case "pengurangan":
+                    total = Convert.ToDouble(a) - Convert.ToDouble(b);
+                    Console.WriteLine("Hasil = {0}", total);
+                    break;
+                case "hasil bagi":
+                    total = Convert.ToDouble(a) % Convert.ToDouble(b);
+                    Console.WriteLine("Hasil = {0}", total);
+                    break;
+            }
+            Console.WriteLine("======================================");
+            Console.WriteLine("---- THANKS YOU <3 ----");
+            Console.WriteLine("======================================");
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
